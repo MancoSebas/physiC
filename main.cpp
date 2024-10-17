330481fe549dc47dd33658c45dc5dfe02483baed
MancoSebas <sebastian.mancom@gmail.com>
2024-10-16 21:24:19 -0500

    Created the FlatVector class
    Create the FlatVector class with some methods like the print and vector
    sum.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "FlatVector.h"
 
 int main(){
 
-  std::cout << "Hello world!" << std::endl;
+  FlatVector FlatVector1(3.0, 4.0);
+  FlatVector FlatVector2(2.0, 6.0);
+ 
+  FlatVector sumVector = FlatVector1 + FlatVector2;
+  
+  std::cout << sumVector << std::endl;
 
   return 0;
 }
