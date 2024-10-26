f22504a1675d8e967fc82e51d90bcaac1b1db792
MancoSebas <sebastian.mancom@gmail.com>
2024-10-25 21:26:08 -0500

    Format changes...

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 
 int main(){
 
-  FlatVector FlatVector1(3.0, 4.0);
-  FlatVector FlatVector2(2.0, 6.0);
- 
-  FlatVector sumVector = FlatVector1 + FlatVector2;
-  
-  std::cout << sumVector << std::endl;
-  std::cout << FlatMath().Length(sumVector) << std::endl; 
-  std::cout << FlatMath().Normalize(sumVector) << std::endl; 
-  std::cout << FlatMath().Dot(FlatVector1,FlatVector2) << std::endl;
+    FlatVector FlatVector1(3.0, 4.0);
+    FlatVector FlatVector2(2.0, 6.0);
+
+    FlatVector sumVector = FlatVector1 + FlatVector2;
+
+    std::cout << sumVector << std::endl;
+    std::cout << FlatMath().Length(sumVector) << std::endl; 
+    std::cout << FlatMath().Normalize(sumVector) << std::endl; 
+    std::cout << FlatMath().Dot(FlatVector1,FlatVector2) << std::endl;
+    std::cout << "Otra Prueba cmake" << endl;
 
   return 0;
  }
